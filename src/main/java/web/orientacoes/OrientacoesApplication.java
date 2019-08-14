816b19dcc7a3213797eaf79af0e25882ed83f054
Mauricio Moia <maununes120@gmail.com>
2019-08-14 10:27:25 -0300

    projeto

---
+++ src/main/java/web/orientacoes/OrientacoesApplication.java
@@ -0,0 +1,13 @@
+package web.orientacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientacoesApplication.class, args);
+	}
+
+}
