459e608547d9bb2ac054955a8bdbbcb3f5bfc2ae
Mauricio Moia <maununes120@gmail.com>
2019-08-19 11:16:17 -0300

    .

--- src/main/java/web/orientacoes/OrientacoesApplication.java
+++ src/main/java/web/orientacoes/OrientacoesApplication.java
@@ -9,7 +9,6 @@ import javax.servlet.DispatcherType;
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
